b8b76bdfd89acce49352d3fa4f2882e18f1891fd
coder-zrl <970586718@qq.com>
2021-08-20 00:05:52 +0800

    使用hutool做定时任务

--- dandelion-server/src/main/java/feidian/cloud/dandelion/DandelionServerApplication.java
+++ dandelion-server/src/main/java/feidian/cloud/dandelion/DandelionServerApplication.java
@@ -1,8 +1,14 @@
 package feidian.cloud.dandelion;
 
+import cn.hutool.core.lang.Console;
+import cn.hutool.cron.CronUtil;
+import cn.hutool.cron.task.Task;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableScheduling
--- dandelion-server/src/main/java/feidian/cloud/dandelion/service/DandelionHeart.java
+++ dandelion-server/src/main/java/feidian/cloud/dandelion/service/DandelionHeart.java
@@ -1,33 +1,52 @@
 package feidian.cloud.dandelion.service;
 
+import cn.hutool.cron.CronUtil;
+import cn.hutool.cron.task.Task;
 import cn.hutool.http.HttpRequest;
-import org.quartz.JobExecutionContext;
-import org.springframework.scheduling.quartz.QuartzJobBean;
 import org.springframework.stereotype.Component;
-
+import javax.annotation.PostConstruct;
 import static feidian.cloud.dandelion.controller.DandelionController.*;
 
+
+/**
+ * @author Zhang Ruilong
+ * @email 970586718@qq.com
+ * @date 2021-08-20 0:02
+ * @description 使用hutool做定时任务
+ */
+
+/**
+ * 注册到spring容器中
+ */
 @Component
-public class DandelionHeart  extends QuartzJobBean {
-    @Override
-    protected void executeInternal(JobExecutionContext jobExecutionContext) {
-        System.out.println("开始心跳检查");
-        for ( String key : idRouteMap.keySet() ) {
-            try {
-                if( idRouteMap.get(key).isClient()) {
-                    String url = idRouteMap.get(key).getUrl();
-                    url = url + "/dandelion/heart";
-                    System.out.println(url);
-                    HttpRequest.post(url)
-                            .timeout(3000)
-                            .execute().body();
+public class DandelionHeart {
+    @PostConstruct
+    public void heart() {
+        CronUtil.schedule("0/5 * * * * ? ", new Task() {
+            @Override
+            public void execute() {
+                System.out.println("开始心跳检查");
+                for ( String key : idRouteMap.keySet() ) {
+                    try {
+                        if( idRouteMap.get(key).isClient()) {
+                            String url = idRouteMap.get(key).getUrl();
+                            url = url + "/dandelion/heart";
+                            System.out.println(url);
+                            HttpRequest.post(url)
+                                    .timeout(3000)
+                                    .execute().body();
+                        }
+                    }catch (Exception e){
+                        idRouteMap.remove(key);
+                    }
                 }
-            }catch (Exception e){
-                idRouteMap.remove(key);
+                System.out.println("结束心跳检查");
+                System.out.println(idRouteMap.keySet());
             }
-        }
-        System.out.println("结束心跳检查");
-        System.out.println(idRouteMap.keySet());
+        });
+        // 支持秒级别定时任务
+        CronUtil.setMatchSecond(true);
+        CronUtil.start();
     }
 }
 
